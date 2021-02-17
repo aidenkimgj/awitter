79862ab2578fcb017f9ddb5692353ae9de65b9c1
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-16 22:57:41 -0700

    Set up firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDUemNUPSiK_k1NyTMMpP5z-IBHunSbqhs',
+  authDomain: 'awitter-5f156.firebaseapp.com',
+  projectId: 'awitter-5f156',
+  storageBucket: 'awitter-5f156.appspot.com',
+  messagingSenderId: '327208526643',
+  appId: '1:327208526643:web:1ef5482214d4fefa2c9446',
+};
+
+export default firebase.initializeApp(firebaseConfig);
