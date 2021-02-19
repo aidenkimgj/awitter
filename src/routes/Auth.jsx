abe8be10f9165d1366ab60271522469953105c0c
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-18 22:54:03 -0700

    Create Auth.jsx

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,4 +1,62 @@
 import React from 'react';
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Button,
+} from 'reactstrap';
 
-const Auth = () => <div>Auth</div>;
+const Auth = () => (
+  <>
+    <Col sm="12" md={{ size: 6, offset: 3 }} className="login">
+      <Card>
+        <CardHeader id="card-header">
+          <strong>Login</strong>
+        </CardHeader>
+        <CardBody>
+          <Form>
+            <FormGroup>
+              <Label for="title">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="email"
+                className="form-control mb-2"
+                required
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="title">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="password"
+                className="form-control"
+                required
+              />
+            </FormGroup>
+
+            <Button color="success" block className="mt-4">
+              Login
+            </Button>
+          </Form>
+        </CardBody>
+        <CardFooter>
+          <Button block className="mt-4 ">
+            Continue with Google
+          </Button>
+          <Button block className=" mb-4">
+            Continue with Github
+          </Button>
+        </CardFooter>
+      </Card>
+    </Col>
+  </>
+);
 export default Auth;
