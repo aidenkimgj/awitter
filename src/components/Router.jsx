8416ea2bd4596f0340f36be06dab2b549d8fa7a7
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-18 22:53:43 -0700

    Add Container into Router.jsx

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -2,19 +2,22 @@ import React, { useState } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../routes/Home';
 import Auth from '../routes/Auth';
+import { Container } from 'reactstrap';
 
 const AppRouter = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <Router>
-      <Switch>
-        {isLoggedIn ? (
-          <Route path="/" exact component={Home} />
-        ) : (
-          <Route path="/" exact component={Auth} />
-        )}
-      </Switch>
-    </Router>
+    <Container id="main-body">
+      <Router>
+        <Switch>
+          {isLoggedIn ? (
+            <Route path="/" exact component={Home} />
+          ) : (
+            <Route path="/" exact component={Auth} />
+          )}
+        </Switch>
+      </Router>
+    </Container>
   );
 };
 
