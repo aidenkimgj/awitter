59e33f0afdf54c6309616bbe6f0d506cab02bb4e
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-18 22:49:07 -0700

    Apply bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import firebase from './firebase';
+import firebase from './fbInstance';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/scss/base.scss';
 import App from './components/App';
 
 console.log(firebase);
