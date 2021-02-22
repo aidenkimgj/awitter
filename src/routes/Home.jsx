482eecb8d1839b810514ce28dcef9676b52f46fe
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-21 22:29:21 -0700

    Create Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,4 +1,38 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Alert } from 'reactstrap';
 
-const Home = () => <div>Home</div>;
+const Home = () => {
+  const getAuroraForecast = async (latitude, longitude) => {
+    const data = await axios.get(
+      `https://api.auroras.live/v1/?type=all&lat=${latitude}&long=${longitude}&forecast=false&threeday=false`
+    );
+    console.log(data);
+  };
+
+  const getLocation = async () => {
+    try {
+      await navigator.geolocation.getCurrentPosition(position => {
+        const {
+          coords: { latitude, longitude },
+        } = position;
+
+        getAuroraForecast(latitude, longitude);
+      });
+    } catch (error) {
+      alert("Can't find you");
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    <>
+      <Link to="/bestlocations">Best Locations</Link>
+    </>
+  );
+};
 export default Home;
