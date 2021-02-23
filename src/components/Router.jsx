377ff8d8a63c4fdb0c83c9486a6473feff9d75ec
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-22 19:23:17 -0700

    Create log out func

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,18 +1,33 @@
 import React, { useState } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import Home from '../routes/Home';
 import Auth from '../routes/Auth';
 import { Container } from 'reactstrap';
+import Navigation from './Navigation';
+import Profile from '../routes/Profile';
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Container id="main-body">
       <Router>
+        {isLoggedIn && <Navigation />}
         <Switch>
           {isLoggedIn ? (
-            <Route path="/" exact component={Home} />
+            <>
+              <Route path="/" exact component={Home} />
+              <Route path="/profile" exact component={Profile} />
+              {/* <Redirect from="*" to="/" /> */}
+            </>
           ) : (
-            <Route path="/" exact component={Auth} />
+            <>
+              <Route path="/" exact component={Auth} />
+              <Redirect from="*" to="/" />
+            </>
           )}
         </Switch>
       </Router>
--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,4 +1,17 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { authService } from '../fbInstance';
 
-const Profile = () => <div>Profile</div>;
+const Profile = () => {
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push('/');
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 export default Profile;
