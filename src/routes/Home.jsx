6f93d5ed0ccf125525e2d7ae8c6939d84f26f28d
Aiden Kim <aidengookjinkim@gmail.com>
2021-02-22 20:40:44 -0700

    Store message in the firestore

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dbService } from '../fbInstance';
 
 const Home = () => {
-  return <>Home</>;
+  const [msg, setMsg] = useState('');
+  const onChange = e => {
+    const {
+      target: { value },
+    } = e;
+    setMsg(value);
+  };
+  const onSubmit = async e => {
+    e.preventDefault();
+    await dbService.collection('aweets').add({
+      msg,
+      createdAt: Date.now(),
+    });
+    setMsg('');
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={msg}
+          placeholder="What's on your mind?"
+          onChange={onChange}
+          maxLength={120}
+        />
+        <input type="submit" value="Aweet" />
+      </form>
+    </>
+  );
 };
 export default Home;
