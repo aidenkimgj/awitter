5619528bd622cd54216af3bb812615406318fd3c
Aiden Kim <aidengookjinkim@gmail.com>
2021-03-01 11:13:00 -0700

    Split away MessageFactory component

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Message from '../components/Message';
 import { dbService, storageService } from '../fbInstance';
-import uuid from 'uuid/dist/v4';
+
+import MessageFactory from '../components/MessageFactory';
 
 const Home = ({ userObj }) => {
-  const [msg, setMsg] = useState('');
   const [msgs, setMsgs] = useState([]);
-  const [image, setImage] = useState('');
 
   /*
   // old way
@@ -34,69 +33,9 @@ const Home = ({ userObj }) => {
     });
   }, []);
 
-  const onChange = e => {
-    const {
-      target: { value },
-    } = e;
-    setMsg(value);
-  };
-
-  const onFileChange = e => {
-    const {
-      target: { files },
-    } = e;
-    const file = files[0];
-    const reader = new FileReader();
-    reader.onloadend = finishedevent => {
-      const {
-        target: { result },
-      } = finishedevent;
-      setImage(result);
-    };
-    reader.readAsDataURL(file);
-  };
-
-  const imageClear = () => setImage('');
-
-  const onSubmit = async e => {
-    e.preventDefault();
-    let imageUrl = '';
-    if (image !== '') {
-      // store attachment in the fire storage
-      const imageRef = storageService.ref().child(`${userObj.uid}/${uuid()}`);
-      const response = await imageRef.putString(image, 'data_url');
-      console.log(response);
-      imageUrl = await response.ref.getDownloadURL();
-    }
-    const msgObj = {
-      text: msg,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-      imageUrl,
-    };
-    await dbService.collection('aweets').add(msgObj);
-    setMsg('');
-  };
-
   return (
     <>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          value={msg}
-          placeholder="What's on your mind?"
-          onChange={onChange}
-          maxLength={120}
-        />
-        <input type="file" accept="image/*" onChange={onFileChange} />
-        <input type="submit" value="Aweet" />
-        {image && (
-          <div>
-            <img src={image} width="50px" height="50px" />
-            <button onClick={imageClear}>Clear</button>
-          </div>
-        )}
-      </form>
+      <MessageFactory userObj={userObj} />
       <div>
         {msgs.map(msg => (
           <Message
